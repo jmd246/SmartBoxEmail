b67207eb04d9b75abd844a10303eb78b815318c4
jmd246 <jdoucette69.jd@gmail.com>
2025-11-24 14:22:52 -0500

    added dependencies, made a helper class to send emails, will need to lookinto oauth2

--- backend/email/src/main/java/App.java
+++ backend/email/src/main/java/App.java
@@ -3,11 +3,14 @@ import io.javalin.Javalin;
 public class App {
     public static void main(String[] args) {
         /*email via gmail smtp*/
-        
+        //need oauth2 and will need to abstract so aany email provider can be used
 
         Javalin app = Javalin.create()
         .get("/", ctx -> ctx.result("Helloworld"))
         .start(7070);
+
+        EmailSender sender = new EmailSender("smtp.gmail.com");
+        sender.sendEmail("jdoucette69.jd@gmail.com","jdoucette69.jd@gmail.com","Test","Test");
         
     }
 }
---
+++ frontend-desktop/demo/src/main/java/com/smartbox_frontend/Main.java
@@ -0,0 +1,7 @@
+package com.smartbox_frontend;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
